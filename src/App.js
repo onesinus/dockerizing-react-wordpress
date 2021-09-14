8620c974ffd69d305834346ef5eefab8352953b1
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-09-14 14:41:49 +0700

    Display data from wp api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8080/?rest_route=/wp/v2/posts')
+      .then(response => response.json())
+      .then(data => setPosts(data))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Gajelas
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Slug</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+      {posts && posts.map(post => (
+        <tr key={post.id}>
+          <td>{post.slug}</td>
+          <td>{post.title.rendered}</td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
   );
 }
 
